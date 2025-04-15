2ef047b5c004b8d3c66e2bbe91f2bb6b5621f85e
Muhammed Ajmal <ajmalkunjammed@gmail.com>
2025-04-15 23:03:07 +0530

    header component completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
+import Jobs from './pages/Jobs'
 function App() {
   return <BrowserRouter>
-      
+      <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signUp' element={<SignUp />} />
         <Route path='/signIn' element={<SignIn />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/browse-jobs' element={<Jobs />} />
         <Route path='/about' element={<About />}/>
         </Routes>
   
