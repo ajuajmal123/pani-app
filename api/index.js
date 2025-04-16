8def86064b182f0c5c25122f845ff888b2e4abc0
Muhammed Ajmal <ajmalkunjammed@gmail.com>
2025-04-16 20:02:29 +0530

    database created

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
 const app=express()
-
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log('DB connected');
+    
+}).catch((err)=>{
+    console.log(err);
+    
+});
 app.listen(3000,()=>{
     console.log('server running on 3000');
     
